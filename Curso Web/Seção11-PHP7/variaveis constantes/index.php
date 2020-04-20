6daa9d1ef9d430b561c05c157af408e83e15d137
diana <diana.iruna@gmail.com>
2020-04-20 09:54:19 -0300

    variaveis constantes

---
+++ Curso Web/Seção11-PHP7/variaveis constantes/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>contantes</title>
+</head>
+<body>
+	 <?php 
+	 	define('BD_URL', 'endereco_bd_dev');
+	 	define('BD_USUARIO', 'usuario_bd_dev');
+	 	define('BD_SENHA', 'senha_bd_dev');
+
+	 	echo 
+
+	 ?>
+
+</body>
+</html>
