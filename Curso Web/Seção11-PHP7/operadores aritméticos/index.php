3dfe8fad47202e2af7b5ca8e99a0c741a4d51806
diana <diana.iruna@gmail.com>
2020-04-20 11:35:38 -0300

    atribuicao com operadores aritmeticos

--- Curso Web/Seção11-PHP7/operadores aritméticos/index.php
+++ Curso Web/Seção11-PHP7/operadores aritméticos/index.php
@@ -10,9 +10,15 @@
 		$num1 = 13;
 		$num2 = 4;
 
-		echo " A soma entre $num1 e $num2 é" . ($num1 + $num2);
+		echo " A soma entre $num1 e $num2 é : " . ($num1 + $num2);
 
+		echo "<br> A subtração entre $num1 e $num2 é : " . ($num1 - $num2);
 
+		echo "<br> A multiplicação entre $num1 e $num2 é : " . ($num1 * $num2);
+
+		echo "<br> A divisão entre $num1 e $num2 é : " . ($num1 / $num2);
+
+		echo "<br> O módulo entre $num1 e $num2 é : " . ($num1 % $num2);
 
 	?>
 
