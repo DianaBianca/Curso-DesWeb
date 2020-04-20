4e22c1160f69b4b62784d4955e0ff6bc3fd1f193
diana <diana.iruna@gmail.com>
2020-04-20 11:30:53 -0300

    praticando com operadores aritméticos

---
+++ Curso Web/Seção11-PHP7/operadores aritméticos/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>operadores aritméticos</title>
+	<meta charset="utf-8">
+
+</head>
+<body>
+	<?php 
+		$num1 = 13;
+		$num2 = 4;
+
+		echo " A soma entre $num1 e $num2 é" . ($num1 + $num2);
+
+
+
+	?>
+
+</body>
+</html>
