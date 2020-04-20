bae0887fcd07d078d1ee68f16fc8103dda964a4a
diana <diana.iruna@gmail.com>
2020-04-20 12:36:09 -0300

    funções

---
+++ Curso Web/Seção11-PHP7/funções/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>funções</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php  
+		//void
+		function exibirBoasVindas(){
+			echo 'Bem-vindo ao curso de PHP !!';
+		}
+
+		exibirBoasVindas();
+
+		//return
+		function calcularAreaTerreno($largura, $comprimmento){
+			$area = $largura * $comprimmento;
+			return 	$area;	
+
+		}
+
+		$resultado = calcularAreaTerreno(5,25);
+		echo $resultado;
+		echo calcularAreaTerreno(15,125);
+		echo "<br>";
+		
+
+	?>
+
+</body>
+</html>
