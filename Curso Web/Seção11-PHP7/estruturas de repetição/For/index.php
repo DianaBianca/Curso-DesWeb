17298c705c3122331f3cabe4cb6c23ddf0accf3c
diana <diana.iruna@gmail.com>
2020-04-22 16:37:24 -0300

    laço de repetição for com array

--- Curso Web/Seção11-PHP7/estruturas de repetição/For/index.php
+++ Curso Web/Seção11-PHP7/estruturas de repetição/For/index.php
@@ -14,6 +14,14 @@
 			echo $i.' X '.$num.' = '.$result.'<br>';
 		}
 
+		$nomes= ['Diana','João','Júlia','Gilvânia','Apolo','João Alberto'];
+
+		$tamanho = count($nomes);
+
+		for ($i=0; $i < $tamanho; $i++) { 
+			echo 'Membro'. $i .' : '.$nomes[$i].'<br>';
+		}
+
 	?>
 
 </body>
