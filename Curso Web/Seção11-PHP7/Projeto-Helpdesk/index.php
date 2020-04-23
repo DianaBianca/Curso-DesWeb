077d0dd874e35b84935f5f9e3d2521a0f7110dde
diana <diana.iruna@gmail.com>
2020-04-23 16:09:28 -0300

    protegendo paginas restritas com session

--- Curso Web/Seção11-PHP7/Projeto-Helpdesk/index.php
+++ Curso Web/Seção11-PHP7/Projeto-Helpdesk/index.php
@@ -49,6 +49,15 @@
                   </div>
 
                 <? }  ?>
+
+                <? if (isset($_GET['login']) && $_GET['login'] == 'erro2') {  ?>
+
+                  <div class="text-danger">
+                    Faça login antes de acessar as paginas protegidas !
+                  </div>
+
+                <? }  ?>
+                
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
--- Curso Web/Seção11-PHP7/Projeto-Helpdesk/valida_login.php
+++ Curso Web/Seção11-PHP7/Projeto-Helpdesk/valida_login.php
@@ -1,7 +1,9 @@
 <?php 
+	session_start();
 
 	//variavel que verifica se a autenticação foi realizada
 	$user_autenticado = false;
+
 	//usuarios do' sistema
 	$usuarios_app = [
 		['email' => 'adm@teste.com.br','senha' => '123456'],
@@ -19,8 +21,9 @@
 
 	if ($user_autenticado) {
 		echo 'Usuario autenticado';
-			
+		$_SESSION['autenticado'] = 'SIM'	;
 	}else{
+		$_SESSION['autenticado'] = 'NAO'	;
 		header('Location: index.php?login=erro');
 	}
 
