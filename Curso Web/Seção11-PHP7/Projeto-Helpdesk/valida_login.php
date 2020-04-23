fb7019eee17e7204de3b61c6a04d7b27deefabd2
diana <diana.iruna@gmail.com>
2020-04-23 15:31:24 -0300

    autenticação de usuários

--- Curso Web/Seção11-PHP7/Projeto-Helpdesk/valida_login.php
+++ Curso Web/Seção11-PHP7/Projeto-Helpdesk/valida_login.php
@@ -1,10 +1,27 @@
 <?php 
-	
-	print_r($_POST);
-	echo "<br>";
-	echo $_POST['email'];
-	echo "<br>";
-	echo $_POST['senha'];
 
+	//variavel que verifica se a autenticação foi realizada
+	$user_autenticado = false;
+	//usuarios do' sistema
+	$usuarios_app = [
+		['email' => 'adm@teste.com.br','senha' => '123456'],
+		['email' => 'user@teste.com.br','senha' => 'asdf'],
+	];
+	
+
+	foreach ($usuarios_app as $user) {
+		if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+
+			$user_autenticado = true;
+			
+		}
+	}
+
+	if ($user_autenticado) {
+		echo 'Usuario autenticado';
+			
+	}else{
+		header('Location: index.php?login=erro');
+	}
 
 ?>
