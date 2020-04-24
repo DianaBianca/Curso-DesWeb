2c0875afa882af135846ffc4d3566cbc198fd23b
diana <diana.iruna@gmail.com>
2020-04-24 01:58:13 -0300

    registrando chamados

--- Curso Web/Seção11-PHP7/Projeto-Helpdesk/abrir_chamado.php
+++ Curso Web/Seção11-PHP7/Projeto-Helpdesk/abrir_chamado.php
@@ -1,5 +1,6 @@
-<?require_once "validador_acesso.php"?>
-
+<? 
+require_once "validador_acesso.php";
+?>
 <html>
   <head>
     <meta charset="utf-8" />
@@ -14,20 +15,21 @@
         margin: 0 auto;
       }
     </style>
-  </head> 
+  </head>
 
   <body>
 
     <nav class="navbar navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
-        <img src="imagens/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
         App Help Desk
       </a>
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="logoff.php">SAIR</a>
+          <a href="logoff.php" class="nav-link">
+          SAIR
+          </a>
         </li>
-        
       </ul>
     </nav>
 
@@ -43,15 +45,15 @@
               <div class="row">
                 <div class="col">
                   
-                  <form>
+                  <form method="post" action="registra_chamado.php">
                     <div class="form-group">
                       <label>Título</label>
-                      <input type="text" class="form-control" placeholder="Título">
+                      <input name="titulo" type="text" class="form-control" placeholder="Título">
                     </div>
                     
                     <div class="form-group">
                       <label>Categoria</label>
-                      <select class="form-control">
+                      <select name="categoria" class="form-control">
                         <option>Criação Usuário</option>
                         <option>Impressora</option>
                         <option>Hardware</option>
@@ -62,16 +64,16 @@
                     
                     <div class="form-group">
                       <label>Descrição</label>
-                      <textarea class="form-control" rows="3"></textarea>
+                      <textarea name="descricao" class="form-control" rows="3"></textarea>
                     </div>
 
                     <div class="row mt-5">
                       <div class="col-6">
-                        <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
+                        <a href="home.php" class="btn btn-lg btn-warning btn-block">Voltar</a>
                       </div>
 
                       <div class="col-6">
-                        <a class="btn btn-lg btn-info btn-block" type="submit">Abrir</button>
+                        <button class="btn btn-lg btn-info btn-block" type="submit">Abrir</button>
                       </div>
                     </div>
                   </form>
---
+++ Curso Web/Seção11-PHP7/Projeto-Helpdesk/registra_chamado.php
@@ -0,0 +1,22 @@
+<?php 
+
+	//trabalhando na montagem do texto
+	$titulo    = str_replace('#', '-', $_POST['titulo']);
+	$categoria =str_replace('#', '-', $_POST['categoria']);
+	$descricao =str_replace('#', '-', $_POST['descricao']);
+
+	//implode('#',$_POST);
+
+	$texto = $_POST['titulo'] .'#'. $_POST['categoria'] .'#'. $_POST['descricao'] . PHP_EOL;
+
+	//abrindo o arquivo
+	$arquivo = fopen('arquivo.hd', 'a');
+	//escrevendo o texto
+	fwrite($arquivo, $texto);
+	//fechando o arquivo
+	fclose($arquivo);
+	//echo $texto;
+
+	header('Location: abrir_chamado.php')
+
+ ?>
