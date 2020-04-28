a1fa2a97175f5809e14bf69082d21a5839af0356
diana <diana.iruna@gmail.com>
2020-04-28 11:23:28 -0300

    comentarios

---
+++ Curso Web/Seção8-Javascript/comentarios/script.js
@@ -0,0 +1,12 @@
+//comentario de uma linha
+//alert('Olá, essa aula é sobre comentarios em JS');
+/*
+	comentario 
+	de 
+	multiplas
+	linhas
+
+
+*/
+
+alert('Olá, essa aula é sobre comentarios em JS');
