99bac2a6c21de9d63d3637e7a181302c32a88657
diana <diana.iruna@gmail.com>
2020-04-28 13:16:23 -0300

    casting

---
+++ Curso Web/Seção8-Javascript/casting/script.js
@@ -0,0 +1,11 @@
+//var var1 = prompt('digite algum numero : ')
+//var var2 = prompt('digite outro numero : ')
+var var1 = 10
+var var2 = 30
+//var1 = parseInt(var1)
+//var2 = parseFloat(var2)
+
+console.log(var1)
+console.log(var2)
+
+document.write(var1.toString() + var2.toString())
