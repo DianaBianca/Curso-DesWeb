50320427f87022380e9aa2240c5dd4948394c417
diana <diana.iruna@gmail.com>
2020-04-28 12:48:27 -0300

    praticando mais com operadores de comparação

--- Curso Web/Seção8-Javascript/if else/script.js
+++ Curso Web/Seção8-Javascript/if else/script.js
@@ -20,3 +20,15 @@ if (idade >= 18) {
 	document.write('criança')
 }
 
+document.write('<br><hr>')
+
+var media = 6
+nota = prompt('digite sua nota')
+
+if (nota > media) {
+	document.write('Aprovado')
+}else if (nota == media) {
+	document.write('passou na risca hein minha filha')
+}else{
+	document.write('não foi dessa vez hein minha filha')
+}
