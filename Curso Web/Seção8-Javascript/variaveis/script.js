3eb59ac73be2ac08a43d4eba637743e23300f2ea
diana <diana.iruna@gmail.com>
2020-04-28 11:37:16 -0300

    variaveis

---
+++ Curso Web/Seção8-Javascript/variaveis/script.js
@@ -0,0 +1,16 @@
+//string
+var texto = 'Curso de Javascript'
+
+//number
+var numeroInteiro = -7
+var numeroFracionado = 111.21
+
+//boolean
+var teste = true;
+var teste1 = false;
+
+alert(numeroFracionado)//abrir um dialog
+
+document.write(texto)//escreve valores dentro do browser
+
+console.log(numeroInteiro)
