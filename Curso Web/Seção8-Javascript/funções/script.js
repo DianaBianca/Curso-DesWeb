3299df31afa5facbae5be43018ed3d632ba7ae32
diana <diana.iruna@gmail.com>
2020-04-29 10:33:19 -0300

    flexibilidade de parametros

--- Curso Web/Seção8-Javascript/funções/script.js
+++ Curso Web/Seção8-Javascript/funções/script.js
@@ -1,6 +1,6 @@
 /*	var area = largura * comprimento
 	return area
-}*/
+}
 
 var largura     = prompt('digite a largura')
 var comprimento = prompt('digite o comprimento')
@@ -13,3 +13,16 @@ function calcularAreaTerreno(largura, comprimento){
 	var area = largura * comprimento
 	return area
 }
+
+*/
+
+
+function soma(a,b){
+	b = b === undefined ? 0: b
+	return a+b
+}
+
+console.log(soma(7,7))
+console.log(soma(7,7,9,15))//desconsidera os demais
+console.log(soma(7))//dá not a number , mas pode ser arrumado na função
+console.log(soma())//dá not a number , mas pode ser arrumado na função
