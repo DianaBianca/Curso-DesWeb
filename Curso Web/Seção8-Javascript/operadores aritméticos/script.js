4cd7a088ac40efff53893ce2fd1befa8ba6b3e29
diana <diana.iruna@gmail.com>
2020-04-29 10:00:58 -0300

    operadores aritméticos

---
+++ Curso Web/Seção8-Javascript/operadores aritméticos/script.js
@@ -0,0 +1,25 @@
+var num1 = 9
+var num2 = 2
+
+
+document.write(	'a soma ente '+num1+' e '+num2+' é : ' + (num1 + num2) )
+document.write('<br>')
+
+document.write('a subtração ente '+num1+' e '+num2+' é : ' + (num1 - num2) )
+document.write('<br>')
+
+document.write('a divisão ente '+num1+' e '+num2+' é : '  + (num1 / num2) )
+document.write('<br>')
+
+document.write('a multiplicação ente '+num1+' e '+num2+' é : ' + (num1 * num2) )
+document.write('<br>')
+
+document.write('a incrementação de '+num1+' é : ' )
+num1++
+document.write(num1 )
+document.write('<br>')
+
+document.write('a decrementação de '+num1+' é : ' )
+num1--
+document.write(num1 )
+document.write('<br>')
