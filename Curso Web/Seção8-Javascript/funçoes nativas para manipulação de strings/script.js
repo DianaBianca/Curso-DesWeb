555fa34fb664c482bf8acf3a164e5b8aafcfc6b7
diana <diana.iruna@gmail.com>
2020-04-30 10:57:54 -0300

    funções de manipulação de string

---
+++ Curso Web/Seção8-Javascript/funçoes nativas para manipulação de strings/script.js
@@ -0,0 +1,32 @@
+//propriedade length - retorna a quantidade de caracteres de uma string
+document.write('Diana Bianca Santos Rodrigues'.length)
+document.write('<br>')
+
+//charAt
+document.write('Diana Bianca Santos Rodrigues'.charAt(2))
+document.write('<br>')
+
+//indexOf
+var nome =  ' Diana Bianca Santos Rodrigues '
+document.write(nome.indexOf('a'))
+document.write('<br>')
+
+//Replace
+document.write(nome.replace('Diana','IRINEU '))
+document.write('<br>')
+
+//substring
+document.write(nome.substr(0,5))
+document.write('<br>')
+
+//to lowercase
+document.write(nome.toUpperCase())
+document.write('<br>')
+
+//to uppercase
+document.write(nome.toLowerCase())
+document.write('<br>')
+
+//trim
+document.write(nome.trim())
+document.write('<br>')
