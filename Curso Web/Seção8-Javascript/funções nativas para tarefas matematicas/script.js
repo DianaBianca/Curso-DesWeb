d6be686af2c5fdd4fd8189aaf73e2470d829064b
diana <diana.iruna@gmail.com>
2020-04-30 11:10:51 -0300

    funções matematicas

---
+++ Curso Web/Seção8-Javascript/funções nativas para tarefas matematicas/script.js
@@ -0,0 +1,21 @@
+var x = 10.580
+
+//arredondando pra cima 
+arre_cima = Math.ceil(x)
+document.write(arre_cima)
+document.write('<br>')
+
+//arredondando pra baixo 
+arre_baixo = Math.floor(x)
+document.write(arre_baixo)
+document.write('<br>')
+
+//arredondando  de acordo com o peso do x
+arre_media= Math.round(x)
+document.write(arre_media)
+document.write('<br>')
+
+//numero aleatorio
+var y = Math.random()
+document.write(y)
+document.write('<br>')
