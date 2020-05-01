3878793ae8759394685d48735b394c8287d223ea
diana <diana.iruna@gmail.com>
2020-05-01 13:33:42 -0300

    DOM manipulando estilos de elementos pt3

--- Curso Web/Seção8-Javascript/DOM/manipulando estilos de elementos/script.js
+++ Curso Web/Seção8-Javascript/DOM/manipulando estilos de elementos/script.js
@@ -2,4 +2,5 @@ function modifica(cor, w, h){
 	document.getElementById('caixa').style.background = cor
 	document.getElementById('caixa').style.width = w
 	document.getElementById('caixa').style.height = h
+
 }
