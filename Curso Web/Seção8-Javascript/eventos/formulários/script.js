a1e1dbce5ca41494e32432f9e5934c504761dc2f
diana <diana.iruna@gmail.com>
2020-05-01 10:53:12 -0300

    eventos-formularios

---
+++ Curso Web/Seção8-Javascript/eventos/formulários/script.js
@@ -0,0 +1,14 @@
+function foco(){
+	console.log('ONFOCUSS')
+	//alert('LOOP') faz um looping infinito pq o foco sai da caixa 
+	//e vai para o botao ok do alert e depois volta para a caixa e em seguida já abre o alert 
+
+}
+
+function blurr(){
+	console.log('ONBLURR')
+}
+
+function change(){
+	console.log('ONCHANGEE')
+}
