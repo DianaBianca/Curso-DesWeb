bead11c605e7cfc869b7256358a36fd9283cd42b
diana <diana.iruna@gmail.com>
2020-05-04 13:26:36 -0300

    laços de repetição - for

---
+++ Curso Web/Seção8-Javascript/estruturas de repetição/for/script.js
@@ -0,0 +1,13 @@
+//for (var i = Things.length - 1; i >= 0; i--) {
+//	Things[i]
+//}
+
+for (var i = 0; i < 10; i++) {
+	document.write(i + '<br>')
+}
+
+var lista = Array('Diana','Bianca','João','Alberto')
+for (var i = 0; i < lista.length; i++) {
+	document.write(lista[i]+'<br>')
+}
+
