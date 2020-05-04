add2829871c5da28051b80d6c24b92f82cdfa175
diana <diana.iruna@gmail.com>
2020-05-04 11:32:23 -0300

    arrays

---
+++ Curso Web/Seção8-Javascript/array/script.js
@@ -0,0 +1,21 @@
+var lista_nome = ['diana',14]
+lista_nome[3] = 'jambolao'
+lista_nome[4] = 1231
+
+console.log(lista_nome)
+
+var lista_frutas = Array()
+lista_frutas[1]='banana'
+lista_frutas[2]='chuchu'
+lista_frutas[3]='uva'
+lista_frutas[4]='melancia'
+
+console.log(lista_frutas)
+
+
+var junto = [lista_frutas,lista_nome]
+
+console.log(junto)
+
+var animais = Array(['gato', 'leao', 'onça'],'elefante', ['cachorro','lobo'],'peixe')
+console.log(animais)
