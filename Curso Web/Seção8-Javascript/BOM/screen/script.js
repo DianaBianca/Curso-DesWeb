ca151406fbd243926af96d1acef56581c039a96e
diana <diana.iruna@gmail.com>
2020-05-05 20:14:34 -0300

    DOM -screen

---
+++ Curso Web/Seção8-Javascript/BOM/screen/script.js
@@ -0,0 +1,16 @@
+var altura = window.screen.availHeight
+var largura = window.screen.availWidth
+
+//var altura = window.screen.height
+//var largura = window.screen.width
+
+//document.write("Altura do navegador é "+ altura)
+//document.write('<br>')
+//document.write("Largura do navegador é "+ largura)
+
+if (largura < 500) {
+	document.write("lógica para impressão do menu versão mobile")
+}else{
+	document.write("lógica para impressão do menu versão web")
+
+}
