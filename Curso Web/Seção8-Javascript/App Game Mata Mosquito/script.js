f285466986e3f296d55f8aae8f0428517334302e
diana <diana.iruna@gmail.com>
2020-05-06 14:52:36 -0300

    App game mosquito- criando e removendo personagem a cada ciclo de tempo

--- Curso Web/Seção8-Javascript/App Game Mata Mosquito/script.js
+++ Curso Web/Seção8-Javascript/App Game Mata Mosquito/script.js
@@ -11,6 +11,12 @@ function ajustaPalcoJogo(){
 ajustaPalcoJogo()
 
 function posicaoRandomica(){
+
+	//remomver a mosca anterior, caso exista
+	if (document.getElementById('mosca')) {
+		document.getElementById('mosca').remove()//removendo o elemento selecionado
+	}
+
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() *  altura) - 90
 
@@ -22,13 +28,17 @@ function posicaoRandomica(){
 
 	//criar o elemento html
 	var mosca = document.createElement('img')
+	
 	mosca.src = 'imagens/mosca.png'
+	
 	mosca.className = tamanhoAleatorio() +' '+ ladoaleatorio()  //atribuindo o elemento para a classe css
 	
 	mosca.style.left = posicaoX + 'px'//acessa atributos de estilo
 	mosca.style.top = posicaoY + 'px'
 	mosca.style.position = 'absolute' // para as coordenadas serem aplicadas, o elemento deve ser absoluto
 	
+	mosca.id = 'mosca'
+
 	document.body.appendChild(mosca) //add um 'filho' para o body
 	
 }
