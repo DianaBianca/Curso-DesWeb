01d58a07336bdea72ba4c3029ef74785a1b020f4
diana <diana.iruna@gmail.com>
2020-05-06 16:17:23 -0300

    App game mosquito- página inicial

--- Curso Web/Seção8-Javascript/App Game Mata Mosquito/script.js
+++ Curso Web/Seção8-Javascript/App Game Mata Mosquito/script.js
@@ -94,3 +94,13 @@ function ladoaleatorio(){
 			return 'ladoB'
 	}
 }
+
+function iniciarJogo(){
+	var nivel = document.getElementById('nivel').value
+
+	if (nivel === '') {
+		alert('Selecione um nível para iniciar o jogo !')
+		return false
+	}
+
+}
