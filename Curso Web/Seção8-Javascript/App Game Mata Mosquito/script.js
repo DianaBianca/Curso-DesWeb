e080464dcf425310e35406c70bccc9c2f5ca9b7c
diana <diana.iruna@gmail.com>
2020-05-06 10:26:16 -0300

    App game mata mosquito - criando posições randomicas

--- Curso Web/Seção8-Javascript/App Game Mata Mosquito/script.js
+++ Curso Web/Seção8-Javascript/App Game Mata Mosquito/script.js
@@ -10,3 +10,24 @@ function ajustaPalcoJogo(){
 
 ajustaPalcoJogo()
 
+function posicaoRandomica(){
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() *  altura) - 90
+
+	// caso a posição seja inferior a 0 = ela recebe o valor de 0
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX,posicaoY)
+
+	//criar o elemento html
+	var mosca = document.createElement('img')
+	mosca.src = 'imagens/mosca.png'
+	mosca.className = 'mosquito1'   //atribuindo o elemento para a classe css
+	
+	mosca.style.left = posicaoX + 'px'//acessa atributos de estilo
+	mosca.style.top = posicaoY + 'px'
+	mosca.style.position = 'absolute' // para as coordenadas serem aplicadas, o elemento deve ser absoluto
+	
+	document.body.appendChild(mosca) //add um 'filho' para o body
+}
