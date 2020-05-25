89ff39da74d5ee61128cffd627b2306ba8ed9d9b
diana <diana.iruna@gmail.com>
2020-05-25 15:45:49 -0300

    contruct e destruct

--- Curso Web/Seção12 - PHP OO/construct_e_destruct.php
+++ Curso Web/Seção12 - PHP OO/construct_e_destruct.php
@@ -1,42 +1,30 @@
 <?php
-    //modelo
-    class Funcionario{
-        //atributos
-        public $nome      = null;
-        public $telefone  = null;
-        public $numFilhos = null;
-        public $cargo     = null;
-        public $salario   = null;
+    class Pessoa{
+        public $nome = null;
 
-        //getters e setters(overloading/sobrecarregar)
+        function __construct ($nome){
+            $this->nome = $nome;
+        }
 
-        function __set($atributo,$valor){
-            $this->$atributo = $valor;
+        function __destruct(){
+            echo 'objeto removido!';
         }
 
         function __get($atributo){
             return $this->$atributo;
-        } 
-
-        //metodos
-        function resumirCadFunc(){
-            return 'Nome : ' . $this->__get('nome') . '<br> Filhos: ' . $this->__get('numFilhos') . '<br> Telefone : ' . $this->__get('telefone') . ' <br> Cargo : ' . $this->__get('cargo') . '<br> Salário : '. $this->__get('salario');
         }
 
-        function modificarNumFilhos($numFilhos){
-            //afetear um atributo do objeto
-            $this->numFilhos = $numFilhos;
+        function correr(){
+            return $this->__get('nome') . ' está correndo';
         }
+   
     }
+    $pessoa = new Pessoa('diana');
+    echo 'Nome: ' . $pessoa->__get('nome');
+    echo  '<br>' . $pessoa->correr() . '<br>';
+    
+    unset($pessoa);//proposital
 
-    $pessoa = new Funcionario();
-    $pessoa->__set('nome','Diana');
-    $pessoa->__set('numFilhos',2);
-    $pessoa->__set('telefone', 12312321);
-    $pessoa->__set('cargo', 'Desenvolvedora Web');
-    $pessoa->__set('salario', 3200);
-    echo $pessoa->__get('nome') . ' possui ' . $pessoa->__get('numFilhos') . ' filhos.';
-    echo '<br>';
-    echo $pessoa->resumirCadFunc();
+    //apos a requisição e finalizada a rsposta, o destruct é executado
 
 ?>
