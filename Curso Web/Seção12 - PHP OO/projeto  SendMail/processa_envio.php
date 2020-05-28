ac278b80d5c3135d4d7b5454df1af50d60f298be
diana <diana.iruna@gmail.com>
2020-05-28 14:50:14 -0300

    enviando emails

--- Curso Web/Seção12 - PHP OO/projeto  SendMail/processa_envio.php
+++ Curso Web/Seção12 - PHP OO/projeto  SendMail/processa_envio.php
@@ -52,20 +52,20 @@
         //Server settings
         $mail->SMTPDebug = 2;                                 // Enable verbose debug output
         $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'user@example.com';                 // SMTP username
-        $mail->Password = 'secret';                           // SMTP password
+        $mail->Username = 'paraqualquercoisamesmo@gmail.com';                 // SMTP username
+        $mail->Password = 'asdzxc546';                           // SMTP password
         $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 587;                                    // TCP port to connect to
 
         //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+        $mail->setFrom('paraqualquercoisamesmo@gmail.com', 'web completo remetente');
+        $mail->addAddress('paraqualquercoisamesmo@gmail.com', 'Web Completo Destinatário');     // Add a recipient
         $mail->addAddress('ellen@example.com');               // Name is optional
-        $mail->addReplyTo('info@example.com', 'Information');
-        $mail->addCC('cc@example.com');
-        $mail->addBCC('bcc@example.com');
+        //$mail->addReplyTo('info@example.com', 'Information');
+        //$mail->addCC('cc@example.com');// destinatario de copia
+        //$mail->addBCC('bcc@example.com');
 
         //Attachments
         $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
