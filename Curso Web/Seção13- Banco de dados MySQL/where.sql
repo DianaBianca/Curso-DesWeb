3b715a74bff1b1be0fed83b98470dd96b68588ab
diana <diana.iruna@gmail.com>
2020-05-29 15:00:22 -0300

    populando o bd com registros pre definidos

---
+++ Curso Web/Seção13- Banco de dados MySQL/where.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS db_curso_web;
+USE db_curso_web;
+
+CREATE TABLE IF NOT EXISTS cursos(
+	id_curso      INT NOT NULL PRIMARY KEY,
+    imagem_curso  VARCHAR(100) NOT NULL, 
+    nome_curso    CHAR(50) NOT NULL,
+    resumo        TEXT NULL,
+    data_cadastro DATETIME NOT NULL,
+    ativo         BOOLEAN DEFAULT TRUE,
+    investimento  FLOAT(5,2)
+);
+
+select * from cursos;
+select id_curso, nome_curso from cursos WHERE id_curso = 1;
+select * from curso where id_curso = 2;
+select * from cursos where investimento < 10.000;
+
