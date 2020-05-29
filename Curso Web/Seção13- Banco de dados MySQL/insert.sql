ade5287b30e43a027910f08ab4ca673d63aadfe3
diana <diana.iruna@gmail.com>
2020-05-29 13:41:28 -0300

    insert

---
+++ Curso Web/Seção13- Banco de dados MySQL/insert.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS db_curso_web;
+USE db_curso_web;
+
+CREATE TABLE IF NOT EXISTS cursos(
+	id_curso      INT NOT NULL PRIMARY KEY,
+    imagem_curso  VARCHAR(100) NOT NULL, 
+    nome_curso    CHAR(50) NOT NULL,
+    resumo        TEXT NULL,
+    data_cadastro DATETIME NOT NULL,
+    ativo         BOOLEAN DEFAULT TRUE,
+    investimento  FLOAT(5,2)
+);
+
+INSERT INTO cursos(id_curso,imagem_curso,nome_curso,resumo,data_cadastro,ativo,investimento) VALUES(1, 'testes','DESENVOLVIMENO.png','aqui nos ensinamos a desenvolver aplicações web','2020-12-22 12:32:12', true, 12.344);
+
