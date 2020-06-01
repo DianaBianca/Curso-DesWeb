daeb5096c20a573e1bc89bf444bfde53d41ba23c
diana <diana.iruna@gmail.com>
2020-05-31 21:35:27 -0300

    projeto loja virtual, populando tabelas

--- Curso Web/Seção13- Banco de dados MySQL/projetoLojavirtual.sql
+++ Curso Web/Seção13- Banco de dados MySQL/projetoLojavirtual.sql
@@ -1,16 +1,25 @@
 CREATE DATABASE IF NOT EXISTS projeto_web;
 USE projeto_web;
 
-create table tb_produtos(
+create table if not exists tb_produtos(
 	id_produto integer primary key not null auto_increment,
     produto varchar(200) not null,
     valor float(8,2) not null
 );
 
-
-create table descricoes_tecnicas(
-	id_descicao integer primary key not null,
+create table if not exists descricoes_tecnicas(
+	id_descicao integer primary key not null auto_increment,
     id_produto integer not null,
     descricao_tecnica text not null,    
-    
+    foreign key(id_produto) references tb_produtos(id_produto)
 );
+
+INSERT INTO tb_produtos(produto, valor) VALUES ('Notebook Dell Inspiron Ultrafino Intel Core i7, 16GB RAM e 240GB SSD', 3500.00);
+
+INSERT INTO tb_produtos(produto, valor) VALUES ('Smart TV LED 40" Samsung Full HD 2 HDMI 1 USB Wi-Fi Integrado', 1475.54);
+
+INSERT INTO tb_produtos(produto, valor) VALUES ('Smartphone LG K10 Dual Chip Android 7.0 4G Wi-Fi Câmera de 13MP', 629.99);
+
+INSERT INTO descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (1, 'O novo Inspiron Dell oferece um design elegante e tela infinita');
+INSERT INTO descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (2, ' A tv da Samsung Full com  Wi-Fi Integrado é perfeita para assistir Stranger Things com a família');
+INSERT INTO descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (3, 'O novo Inspiron Smartphone LG K10 Dual Chip é capaz de registrar lindos momentos com sua super camera de 13mp');
