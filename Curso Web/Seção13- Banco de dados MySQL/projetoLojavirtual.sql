7e4d6ebd71466c59145164995ccdc1a06f579067
diana <diana.iruna@gmail.com>
2020-06-01 20:30:38 -0300

    relacionamento muitos para muitos

--- Curso Web/Seção13- Banco de dados MySQL/projetoLojavirtual.sql
+++ Curso Web/Seção13- Banco de dados MySQL/projetoLojavirtual.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS projeto_web;
 USE projeto_web;
 
-create table if not exists tb_produtos(
-	id_produto integer primary key not null auto_increment,
+create table if not exists produtos(
+	id_produto int primary key not null auto_increment,
     produto varchar(200) not null,
     valor float(8,2) not null
 );
@@ -11,14 +11,14 @@ create table if not exists descricoes_tecnicas(
 	id_descicao integer primary key not null auto_increment,
     id_produto integer not null,
     descricao_tecnica text not null,    
-    foreign key(id_produto) references tb_produtos(id_produto)
+    foreign key(id_produto) references produtos(id_produto)
 );
 
-INSERT INTO tb_produtos(produto, valor) VALUES ('Notebook Dell Inspiron Ultrafino Intel Core i7, 16GB RAM e 240GB SSD', 3500.00);
+INSERT INTO produtos(produto, valor) VALUES ('Notebook Dell Inspiron Ultrafino Intel Core i7, 16GB RAM e 240GB SSD', 3500.00);
 
-INSERT INTO tb_produtos(produto, valor) VALUES ('Smart TV LED 40" Samsung Full HD 2 HDMI 1 USB Wi-Fi Integrado', 1475.54);
+INSERT INTO produtos(produto, valor) VALUES ('Smart TV LED 40" Samsung Full HD 2 HDMI 1 USB Wi-Fi Integrado', 1475.54);
 
-INSERT INTO tb_produtos(produto, valor) VALUES ('Smartphone LG K10 Dual Chip Android 7.0 4G Wi-Fi Câmera de 13MP', 629.99);
+INSERT INTO produtos(produto, valor) VALUES ('Smartphone LG K10 Dual Chip Android 7.0 4G Wi-Fi Câmera de 13MP', 629.99);
 
 INSERT INTO descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (1, 'O novo Inspiron Dell oferece um design elegante e tela infinita');
 INSERT INTO descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (2, ' A tv da Samsung Full com  Wi-Fi Integrado é perfeita para assistir Stranger Things com a família');
@@ -27,7 +27,7 @@ INSERT INTO descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (3, 'O nov
 create table if not exists imagens(
 	id_imagem int not null auto_increment primary key,
     id_produto int not null,
-    foreign key(id_produto) references tb_produtos(id_produto),
+    foreign key(id_produto) references produtos(id_produto),
     url_imagem varchar(200) not null #endereço da imagem fica armazenada no banco
 );
 
@@ -35,23 +35,25 @@ insert into imagens(id_produto,url_imagem) values (1,'notebook1.jpg'),(1,'notebo
 insert into imagens(id_produto,url_imagem) values (2,'smarttv1.jpg'),(2,'smarttv2.jpg');
 insert into imagens(id_produto,url_imagem) values (3,'smatfone1.jpg');
 
-create table if not exists tb_clientes(
+create table if not exists clientes(
 	id_cliente int not null primary key auto_increment,
     nome varchar(100) not null,
     idade int(3) not null
 );
 
-create table if not exists tb_pedidos(
+create table if not exists pedidos(
 	id_pedido int not null primary key auto_increment,
     id_cliente int not null,
     data_hora datetime not null default current_timestamp,
-    foreign key(id_cliente) references tb_clientes(id_clientes)
+    FOREIGN KEY(id_cliente) REFERENCES clientes(id_clientes)
 );
 
 create table if not exists tb_pedidos_produtos(
 	id_pedido int not null,
-    id_produto integer not null,
-    foreign key(id_pedido)  references tb_pedidos (id_pedido),
-    foreign key(id_produto) references tb_produtos (id_produto)
+    id_produto int not null,
+    constraint foreign key(id_produto) references produtos(id_produto),
+    constraint foreign key(id_pedido) references pedidos(id_pedido)
 );
+
+
 
