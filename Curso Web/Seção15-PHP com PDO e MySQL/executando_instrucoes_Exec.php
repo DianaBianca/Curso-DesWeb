357271dd3a9836c6ff2bcadd060a39e16cf8aaa0
diana <diana.iruna@gmail.com>
2020-06-05 15:59:03 -0300

    php com Exec

--- Curso Web/Seção15-PHP com PDO e MySQL/executando_instrucoes_Exec.php
+++ Curso Web/Seção15-PHP com PDO e MySQL/executando_instrucoes_Exec.php
@@ -11,12 +11,24 @@
         $query = '
             create table if not exists tb_usuario(
                 id    int not null primary key auto_increment,
-                nome  varchar(50) not null,
+                nome  varchar(50)  not null,
                 email varchar(100) not null,
-                senha varchar(32) not null
+                senha varchar(32)  not null
 
             );
-        '
+        ';
+
+        $retorno = $conexao->exec($query);//executa qualquer tipobde query e retorna o num de linhas afetadas
+        //0
+        echo $retorno;
+
+        $query = '
+            insert into tb_usuario(nome, email,senha)
+            values ("Joao alberto R", "joaoa123@gmail.com", "123321");
+        ';
+        $retorno = $conexao->exec($query);
+        echo $retorno;
+
 
     }catch(PDOException $e){
         echo 'Erro: '.$e->getCode().' Mensagem: '.getMessage();
