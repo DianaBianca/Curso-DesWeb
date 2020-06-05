2bd4ac5c37a1891376abb5b979cdc588832ea073
diana <diana.iruna@gmail.com>
2020-06-05 16:13:42 -0300

    pdoStatement com FetchAll

--- Curso Web/Seção15-PHP com PDO e MySQL/pdo_com_fetchAll.php
+++ Curso Web/Seção15-PHP com PDO e MySQL/pdo_com_fetchAll.php
@@ -7,50 +7,14 @@
 
     try{
         $conexao = new PDO($dsn,$usuario,$senha );
-        /*
-        $query = '
-            create table if not exists tb_usuario(
-                id    int not null primary key auto_increment,
-                nome  varchar(50)  not null,
-                email varchar(100) not null,
-                senha varchar(32)  not null
+       
+        $query   = 'select * from tb_usuario;';
 
-            );
-        ';
+        $stmt    = $conexao->query($query);
+        $lista   = $stmt->fetchAll();//retorna todos os registros recuperados da consulta
+        print_r($lista);
 
-        $retorno = $conexao->exec($query);//executa qualquer tipobde query e retorna o num de linhas afetadas
-        //0
-        //echo $retorno;
-        
-        $query = '
-            insert into tb_usuario(nome, email,senha)
-            values ("Joao alberto R", "joaoa123@gmail.com", "123321");
-        ';
-        $retorno = $conexao->exec($query);
-        echo $retorno;
-        */
-
-        $query = '
-            insert into tb_usuario(nome, email,senha)
-            values("Joao alberto R", "joaoa123@gmail.com", "123321");
-        ';
-        $retorno = $conexao->exec($query);
-        
-
-        $query = '
-            insert into tb_usuario(nome, email,senha)
-            values("Julia Marina", "ujuuju@gmail.com", "12juju");
-        ';
-        $retorno = $conexao->exec($query);
-        
-
-        $query = '
-            insert into tb_usuario(nome, email,senha)
-            values("Janaina Ribeiro", "janaah@gmail.com", "nahnahjana");
-        ';
-        $retorno = $conexao->exec($query);
-        
-
+        echo $lista[0]['nome'];
 
     }catch(PDOException $e){
         echo 'Erro: '.$e->getCode().' Mensagem: '.getMessage();
