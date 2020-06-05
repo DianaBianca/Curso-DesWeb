14ece381466ba8665b6c2c4bf216464ab91c9d8c
diana <diana.iruna@gmail.com>
2020-06-05 16:53:31 -0300

    retornando um unico registro

--- Curso Web/Seção15-PHP com PDO e MySQL/retornando_um_unico_registro.php
+++ Curso Web/Seção15-PHP com PDO e MySQL/retornando_um_unico_registro.php
@@ -8,19 +8,19 @@
     try{
         $conexao = new PDO($dsn,$usuario,$senha );
        
-        $query   = 'select * from tb_usuario;';
-
-        $stmt    = $conexao->query($query);
-        #$lista   = $stmt->fetchAll(PDO::FETCH_ASSOC);//fetch associativo
-        #$lista   = $stmt->fetchAll(PDO::FETCH_NUM);
-        #$lista   = $stmt->fetchAll(PDO::FETCH_BOTH);
-        $lista   = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $query   = '
+            select * from tb_usuario order by nome desc limit 1;
+        ';
+        
+        $stmt      = $conexao->query($query);
+        $usuario   = $stmt->fetch(PDO::FETCH_ASSOC);#fetch retorna um unico registro
 
         echo '<pre>';
-        print_r($lista);
+        print_r($usuario);
         echo '</pre>';
 
-        #echo $lista[2]->['nome'];
+        #echo $usuario->nome;
+        echo $usuario['nome'];
 
     }catch(PDOException $e){
         echo 'Erro: '.$e->getCode().' Mensagem: '.getMessage();
