9ae84bb6084655032ff6d131b0869850f08a1585
diana <diana.iruna@gmail.com>
2020-06-05 17:01:57 -0300

    listando registros com foreach

--- Curso Web/Seção15-PHP com PDO e MySQL/listando_registros_foreach.php
+++ Curso Web/Seção15-PHP com PDO e MySQL/listando_registros_foreach.php
@@ -9,18 +9,29 @@
         $conexao = new PDO($dsn,$usuario,$senha );
        
         $query   = '
-            select * from tb_usuario order by nome desc limit 1;
+            select * from tb_usuario ;
         ';
         
-        $stmt      = $conexao->query($query);
-        $usuario   = $stmt->fetch(PDO::FETCH_ASSOC);#fetch retorna um unico registro
+        //$stmt      = $conexao->query($query);
+        foreach($conexao->query($query) as $key => $value){
+            echo $value['nome'];
+            echo '<hr>';
+        }
 
-        echo '<pre>';
-        print_r($usuario);
-        echo '</pre>';
+        //$lista_usuarios   = $stmt->fetchAll(PDO::FETCH_ASSOC);#fetch retorna um unico registro
+
+        //echo '<pre>';
+        //print_r($lista_usuarios);
+        //echo '</pre>';
 
         #echo $usuario->nome;
-        echo $usuario['nome'];
+        #echo $lista_usuarios['nome'];
+        /*
+        foreach($lista_usuarios as $key => $value){
+            echo $value['nome'];
+            echo '<hr>';
+        }
+        */
 
     }catch(PDOException $e){
         echo 'Erro: '.$e->getCode().' Mensagem: '.getMessage();
