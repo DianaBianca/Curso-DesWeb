f501040f3c50bf2ad4797f447502305d1e9806b6
diana <diana.iruna@gmail.com>
2020-06-08 19:40:13 -0300

    app lista de tarefas

---
+++ Curso Web/Seção15-PHP com PDO e MySQL/app_lista_tarefas/tarefa.service.php
@@ -0,0 +1,26 @@
+<?php
+//crud
+     class TarefaService{
+
+        public function inserir(){//crate
+
+        }
+
+        public function recuperar(){//read
+
+        }
+
+        public function atualizar(){//update
+
+        }
+
+        public function remover(){//delete
+
+        }
+
+
+     }
+
+
+?>
+
