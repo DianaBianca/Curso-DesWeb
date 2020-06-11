30c1b2748f706067a55f8d4bac678ebeb5ed99e9
diana <diana.iruna@gmail.com>
2020-06-11 19:58:29 -0300

    atualizando os registros pt 2

--- Curso Web/Seção15-PHP com PDO e MySQL/app_lista_tarefas/tarefa_controller.php
+++ Curso Web/Seção15-PHP com PDO e MySQL/app_lista_tarefas/tarefa_controller.php
@@ -26,7 +26,17 @@
         $tarefaService = new TarefaService($conexao,$tarefa);
         $tarefas = $tarefaService->recuperar();
 
+    }else if($acao == 'atualizar'){
 
+        $tarefa = new Tarefa();
+        $tarefa-> __set('id', $_POST['id']);
+        $tarefa-> __set('tarefa', $_POST['tarefa']);
+
+
+        $conexao = new Conexao();
+
+        $tarefaService = new TarefaService($conexao, $tarefa);
+        $tarefaService->atualizar(); 
 
     }
 
