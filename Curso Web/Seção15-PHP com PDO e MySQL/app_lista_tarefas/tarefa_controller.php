c70004cd20c0d0eca603fb004f866df425cce5d4
diana <diana.iruna@gmail.com>
2020-06-11 15:34:42 -0300

    listando os registros

--- Curso Web/Seção15-PHP com PDO e MySQL/app_lista_tarefas/tarefa_controller.php
+++ Curso Web/Seção15-PHP com PDO e MySQL/app_lista_tarefas/tarefa_controller.php
@@ -4,15 +4,25 @@
     require "tarefa.service.php";
     require "conexao.php";
 
-    
+    $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
-    $tarefa = new Tarefa();
-    $tarefa->__set('tarefa',$_POST['tarefa']);
+    if($acao == 'inserir'){
 
-    $conexao = new Conexao();
+        $tarefa = new Tarefa();
+        $tarefa->__set('tarefa',$_POST['tarefa']);
 
-    $tarefaService = new TarefaService($conexao,$tarefa);
-    $tarefaService->inserir();
-     
-    header('Location: nova_tarefa.php?inclusao=1');
+        $conexao = new Conexao();
+
+        $tarefaService = new TarefaService($conexao,$tarefa);
+        $tarefaService->inserir();
+        
+        header('Location: nova_tarefa.php?inclusao=1');
+
+    }else if($acao == 'recuperar'){
+        echo 'aqui';
+    }
+
+
+
+
 ?>
