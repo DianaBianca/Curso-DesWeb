dc5540255299d5e9869a62fc56961789bb509400
diana <diana.iruna@gmail.com>
2020-06-25 11:55:29 -0300

    jquery - selecionando elementos pt 2

---
+++ Curso Web/Secção17-jQuery/selecionando elementos/script.js
@@ -0,0 +1,24 @@
+ /*
+    $(document).ready(function(){
+        function teste(){
+            console.log($('#exemplo'))
+       
+        }
+        teste()
+    })
+
+    OU
+
+    function teste(){
+        console.log($('#exemplo'))
+   
+    }
+    $(teste)
+
+    OU
+    */
+
+
+   $(function(){
+    console.log($('#exemplo'))
+})
