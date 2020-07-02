e87cae68aa59d204607727582e2895030bf293ef
diana <diana.iruna@gmail.com>
2020-07-02 14:42:36 -0300

    preparando o back-end da aplicação

--- Curso Web/Secção17-jQuery/projeto/app_dashboard/app.php
+++ Curso Web/Secção17-jQuery/projeto/app_dashboard/app.php
@@ -24,20 +24,21 @@
         private $user = 'root';
         private $pass = '';
 
-        try{
-
-            $conexao = new PDO(
-                "mysql:host=$this->host;dbmane=$this->dbname",
-                "$this->user",
-                "$this->pass"
-            );
-
-            //instancia da conexao trabalhando com UTF8
-            $conexao->exec('set charset set utf8');
-            return $conexao;
-
-        }catch(PDOException $e){
-            echo '<p>'.$e->getMessage().'</p>';
+        public function conectar(){
+            try{
+                $conexao = new PDO(
+                    "mysql:host=$this->host;dbmane=$this->dbname",
+                    "$this->user",
+                    "$this->pass"
+                );
+    
+                //instancia da conexao trabalhando com UTF8
+                $conexao->exec('set charset set utf8');
+                return $conexao;
+    
+            }catch(PDOException $e){
+                echo '<p>'.$e->getMessage().'</p>';
+            }
         }
     }
     //class (model)
