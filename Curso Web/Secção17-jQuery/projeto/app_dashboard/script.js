eed753361ac474b04137f35a86bc7a9185a52d18
diana <diana.iruna@gmail.com>
2020-07-02 16:31:17 -0300

    utilizando o metodo $.ajax()

--- Curso Web/Secção17-jQuery/projeto/app_dashboard/script.js
+++ Curso Web/Secção17-jQuery/projeto/app_dashboard/script.js
@@ -1,57 +1,49 @@
 $(document).ready(() => {
-	$('#documentacao').on('click', () =>{
-        //$('#pagina').load('documentacao.html')//carrega a pagina html , faz requisção get
-        /*
-        $.get('documentacao.html',valor =>{
-            console.log(valor)
-            $('#pagina').html(valor)
+	
+	$('#documentacao').on('click', () => {
+		//$('#pagina').load('documentacao.html')
 
-        })
-        */
+		/*
+		$.get('documentacao.html', data => { 
+			$('#pagina').html(data)
+		})
+		*/
+		$.post('documentacao.html', data => { 
+			$('#pagina').html(data)
+		})
+	})
 
-       $.post('documentacao.html',valor =>{
-        console.log(valor)
-        $('#pagina').html(valor)
+	$('#suporte').on('click', () => {
+		//$('#pagina').load('suporte.html')
 
-    })
-
-    })
-    $('#suporte').on('click', () =>{
-        /*
-        //$('#pagina').load('suporte.html')//carrega a pagina html 
-
-        $.get('suporte.html',valor =>{
-            console.log(valor)
-            $('#pagina').html(valor)
-
-        })
-        */
-       $.get('suporte.html',valor =>{
-        console.log(valor)
-        $('#pagina').html(valor)
-
-        })
-    })
-
-    //ajax
-    $('competencia').on('change',e =>{
-        //console.log($(e.target).val())
-        let competencia = $(e.target).val()
-        
-        $.ajax({
-            type:'GET',
-            url: 'app.php',
-            data:`competencia=${competencia}`,//x-www-form-urlencoded
-            dataType:'json',
-            success: (dados) =>{
-                $('#numeroVendas').html(dados.numeroVendas)
-                $('#totalVendas').html(dados.toalVendas)
-            },
-            error:(erro) => {console.log(erro)}
-        })
-
-        //metodo, url,dados, sucesso,erro
-    })
+		/*
+		$.get('suporte.html', data => { 
+			$('#pagina').html(data)
+		})
+		*/
+		$.post('suporte.html', data => { 
+			$('#pagina').html(data)
+		})
+	})
 
 
+	//ajax
+	$('#competencia').on('change', e => {
+
+		let competencia = $(e.target).val()
+		
+		$.ajax({
+			type: 'GET',
+			url: 'app.php',
+			data: `competencia=${competencia}`, //x-www-form-urlencoded
+			dataType: 'json',
+			success: dados => { 
+				$('#numeroVendas').html(dados.numeroVendas)
+				$('#totalVendas').html(dados.totalVendas)
+			},
+			error: erro => { console.log(erro) }
+		})
+
+		//método, url, dados, sucesso, erro
+	})
 })
