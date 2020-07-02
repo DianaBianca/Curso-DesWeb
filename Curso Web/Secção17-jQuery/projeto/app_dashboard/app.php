e0d40706a49935fce24df1566cea51ff44a86ccf
diana <diana.iruna@gmail.com>
2020-07-02 15:25:18 -0300

    preparando o back-end da aplicação

--- Curso Web/Secção17-jQuery/projeto/app_dashboard/app.php
+++ Curso Web/Secção17-jQuery/projeto/app_dashboard/app.php
@@ -1,106 +1,110 @@
 <?php
 
-    //classe dashborad
-    class Dashboard{
-        public $data_inicio;
-        public $data_fim;
-        public $numeroVendas;
-        public $totalVendas;
+//classe dashboard
+class Dashboard {
+
+	public $data_inicio;
+	public $data_fim;
+	public $numeroVendas;
+	public $totalVendas;
+
+	public function __get($atributo) {
+		return $this->$atributo;
+	}
+
+	public function __set($atributo, $valor) {
+		$this->$atributo = $valor;
+		return $this;
+	}
+}
+
+//classe de conexÃ£o bd
+class Conexao {
+	private $host = 'localhost';
+	private $dbname = 'dashboard';
+	private $user = 'root';
+	private $pass = '';
+
+	public function conectar() {
+		try {
+
+			$conexao = new PDO(
+				"mysql:host=$this->host;dbname=$this->dbname",
+				"$this->user",
+				"$this->pass"
+			);
+
+			//
+			$conexao->exec('set charset set utf8');
+
+			return $conexao;
+
+		} catch (PDOException $e) {
+			echo '<p>'.$e->getMessege().'</p>';
+		}
+	}
+}
+
+//classe (model)
+class Bd {
+	private $conexao;
+	private $dashboard;
+
+	public function __construct(Conexao $conexao, Dashboard $dashboard) {
+		$this->conexao = $conexao->conectar();
+		$this->dashboard = $dashboard;
+	}
+
+	public function getNumeroVendas() {
+		$query = '
+			select 
+				count(*) as numero_vendas 
+			from 
+				tb_vendas 
+			where 
+				data_venda between :data_inicio and :data_fim';
+
+		$stmt = $this->conexao->prepare($query);
+		$stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+		$stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+		$stmt->execute();
+
+		return $stmt->fetch(PDO::FETCH_OBJ)->numero_vendas;
+	}
+
+	public function getTotalVendas() {
+		$query = '
+			select 
+				SUM(total) as total_vendas 
+			from 
+				tb_vendas 
+			where 
+				data_venda between :data_inicio and :data_fim';
+
+		$stmt = $this->conexao->prepare($query);
+		$stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+		$stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+		$stmt->execute();
+
+		return $stmt->fetch(PDO::FETCH_OBJ)->total_vendas;
+	}
+}
 
 
-        public function __get($atributo){
-            return $this.atributo;
-        }
+//lÃ³gica do script
+$dashboard = new Dashboard();
 
-        public function __set($atributo,$valor){
-            $this->$atributo = $valor;
-            return $this;
-        }
-    }
+$conexao = new Conexao();
 
-    class Conexao{
-        private $host = 'localhost';
-        private $dbname = 'dashboard';
-        private $user = 'root';
-        private $pass = '';
-
-        public function conectar(){
-            try{
-                $conexao = new PDO(
-                    "mysql:host=$this->host;dbmane=$this->dbname",
-                    "$this->user",
-                    "$this->pass"
-                );
-    
-                //instancia da conexao trabalhando com UTF8
-                $conexao->exec('set charset set utf8');
-                return $conexao;
-    
-            }catch(PDOException $e){
-                echo '<p>'.$e->getMessage().'</p>';
-            }
-        }
-    }
-    //class (model)
-    class Bd{
-
-        private $conexao;
-        private $dashboard;
-
-        public function __construct(Conexao $conexao, Dashboard $dashboard){
-            $this->conexao = $conexao->conectar();
-            $this->dashboard = $dashboard;
-
-        }
-
-        public function getNumeroVendas(){
-            $query = 'select 
-                        count(*) as numero_vendas
-                      from
-                        tb_vendas
-                      where
-                        data_venda between :data_inicio anda :data_fim ';
-
-            $stmt = $this->conexao->prepare($query);
-            $stmt->bindValue(':data_inicio',$this->dashboard->__get('data_inicio'));
-            $stmt->bindValue(':data_fim',$this->dashboard->__get('data_fim'));
-            $stmt->execute();
-
-            return ($stmt->fetch(PDO::FETCH_OBJ)->numero_vendas);
-
-        }
-
-        public function getTotalVendas(){
-            $query = 'select 
-                        sum(total) as total_vendas
-                      from
-                        tb_vendas
-                      where
-                        data_venda between :data_inicio anda :data_fim ';
-
-            $stmt = $this->conexao->prepare($query);
-            $stmt->bindValue(':data_inicio',$this->dashboard->__get('data_inicio'));
-            $stmt->bindValue(':data_fim',$this->dashboard->__get('data_fim'));
-            $stmt->execute();
-
-            return ($stmt->fetch(PDO::FETCH_OBJ)->total_vendas);
-
-        }
+$dashboard->__set('data_inicio', '2018-10-01');
+$dashboard->__set('data_fim', '2018-10-31');
 
 
-    }
-    $dashboard = new Dashboard();
-    $conexao   = new Conexao();
+$bd = new Bd($conexao, $dashboard);
+
+$dashboard->__set('numeroVendas', $bd->getNumeroVendas());
+$dashboard->__set('totalVendas', $bd->getTotalVendas());
+print_r($dashboard);
 
 
-    $dashboard->__set('data_inicio','2018-08-01');
-    $dashboard->__set('data_inicio','2018-10-31');
-
-    $bd = new Bd($conexao,$dashboard);
-
-    $dashboard->__set('numeroVendas', $bd->getNumeroVendas());
-    $dashboard->__set('totalVendas', $bd->getTotalVendas());
-    print_r($dashboard);
-
-    
 ?>
