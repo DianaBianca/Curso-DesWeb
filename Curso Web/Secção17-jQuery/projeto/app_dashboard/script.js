a1ca53f90abd08463d4aa4c04e95b77ef0c07c7d
diana <diana.iruna@gmail.com>
2020-07-02 13:00:28 -0300

    projrto de fim de moodulo - requisicoes para o back-end

--- Curso Web/Secção17-jQuery/projeto/app_dashboard/script.js
+++ Curso Web/Secção17-jQuery/projeto/app_dashboard/script.js
@@ -1,3 +1,10 @@
 $(document).ready(() => {
-	
+	$('#documentacao').on('click', () =>{
+        console.log('link documentação clicado')
+
+    })
+    $('#suporte').on('click', () =>{
+        console.log('link suporte clicado')
+        
+    })
 })
