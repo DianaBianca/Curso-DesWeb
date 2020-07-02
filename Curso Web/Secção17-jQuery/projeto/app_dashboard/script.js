f4cdbc7c3cb7a49820d8372d69e30b511578f637
diana <diana.iruna@gmail.com>
2020-07-02 14:23:03 -0300

    preparando o back-end da aplicação

--- Curso Web/Secção17-jQuery/projeto/app_dashboard/script.js
+++ Curso Web/Secção17-jQuery/projeto/app_dashboard/script.js
@@ -1,10 +1,35 @@
 $(document).ready(() => {
 	$('#documentacao').on('click', () =>{
-        $('#pagina').load('documentacao.html')//carrega a pagina html 
+        //$('#pagina').load('documentacao.html')//carrega a pagina html , faz requisção get
+        /*
+        $.get('documentacao.html',valor =>{
+            console.log(valor)
+            $('#pagina').html(valor)
+
+        })
+        */
+
+       $.post('documentacao.html',valor =>{
+        console.log(valor)
+        $('#pagina').html(valor)
+
+    })
 
     })
     $('#suporte').on('click', () =>{
-        $('#pagina').load('suporte.html')//carrega a pagina html 
-        
+        /*
+        //$('#pagina').load('suporte.html')//carrega a pagina html 
+
+        $.get('suporte.html',valor =>{
+            console.log(valor)
+            $('#pagina').html(valor)
+
+        })
+        */
+       $.get('suporte.html',valor =>{
+        console.log(valor)
+        $('#pagina').html(valor)
+
+        })
     })
 })
