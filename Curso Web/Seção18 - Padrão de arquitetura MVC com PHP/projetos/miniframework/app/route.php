b99e554264a62f8cc6ad91e1c98fde3a6e0e1abd
diana <diana.iruna@gmail.com>
2020-07-07 14:28:18 -0300

    iniciando a configuração de  rotas do miniframework DONE

--- Curso Web/Seção18 - Padrão de arquitetura MVC com PHP/projetos/miniframework/app/route.php
+++ Curso Web/Seção18 - Padrão de arquitetura MVC com PHP/projetos/miniframework/app/route.php
@@ -2,12 +2,23 @@
 namespace app;
 
 class Route{
-
+    public function initRoutes(){
+        $routes['home']= array(
+            'route' => '/',
+            'controller' => 'indexController',
+            'action' => 'index'
+        );
+        $routes['sobre_nos'] = array(
+            'route' => '/',
+            'controller' => 'indexController',
+            'action' => 'sobreNos'
+        );
+    }
     public function getUrl(){
         #return $_SERVER;//retorna todos os detalhaes do servidor da aplicação
         #return parse_url($_SERVER['REQUEST_URI']);//retorna um array detalhando os componentes da URL
         return parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);//retorna a string relativa ao path
-        
+
     }
 }
 
