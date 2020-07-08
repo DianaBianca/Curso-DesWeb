79fc12ddaef86c3a98a093d88641cfe7a8b2f31c
diana <diana.iruna@gmail.com>
2020-07-07 21:34:31 -0300

    abstração da classe route

--- Curso Web/Seção18 - Padrão de arquitetura MVC com PHP/projetos/miniframework/app/route.php
+++ Curso Web/Seção18 - Padrão de arquitetura MVC com PHP/projetos/miniframework/app/route.php
@@ -1,63 +1,23 @@
 <?php
-namespace app;
+    namespace app;
+    use MF\Init\Bootstrap;
 
-class Route{
+    class Route extends Bootstrap{
 
-    private $routes;
+        protected function initRoutes(){
+            $routes['home']= array(
+                'route' => '/',
+                'controller' => 'indexController',
+                'action' => 'index'
+            );
+            $routes['sobre_nos'] = array(
+                'route' => '/sobre_nos',
+                'controller' => 'indexController',
+                'action' => 'sobreNos'
+            );
 
-    public function __construct(){
-        $this->initRoutes();
-        $this->run($this->getUrl());
-    }
-
-    public function getRoutes(){
-        return $this->routes;
-    }
-
-    public function setRoutes(array $routes){
-        $this->routes = $routes;
-    }
-
-    public function initRoutes(){
-        $routes['home']= array(
-            'route' => '/',
-            'controller' => 'indexController',
-            'action' => 'index'
-        );
-        $routes['sobre_nos'] = array(
-            'route' => '/sobre_nos',
-            'controller' => 'indexController',
-            'action' => 'sobreNos'
-        );
-
-        $this->setRoutes($routes); //inicialização do array de rotas
-    }
-
-    public function run($url){
-
-        //echo "*********".$url."**********";
-        foreach( $this->getRoutes() as $key => $route){
-            
-            if($url == $route['route']){
-                $class = "app\\controllers\\".$route['controller'];//ucfirst($route['controller']);
-                
-                $controller = new $class;
-
-                //actions
-                $action = $route['action'];
-
-                $controller->$action();
-            }
+            $this->setRoutes($routes); //inicialização do array de rotas
         }
-
     }
-
-    public function getUrl(){
-        #return $_SERVER;//retorna todos os detalhaes do servidor da aplicação
-        #return parse_url($_SERVER['REQUEST_URI']);//retorna um array detalhando os componentes da URL
-        return parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);//retorna a string relativa ao path
-
-    }
-}
 
 ?>
--- Curso Web/Seção18 - Padrão de arquitetura MVC com PHP/projetos/miniframework/vendor/MF/Init/Bootstrap.php
+++ Curso Web/Seção18 - Padrão de arquitetura MVC com PHP/projetos/miniframework/vendor/MF/Init/Bootstrap.php
@@ -2,7 +2,49 @@
     namespace MF\Init;
 
     abstract class Bootstrap{//nao pode ser instanciada, somente herdada
-   
+        private $routes;
+
+        abstract protected function initRoutes();
+
+        public function __construct(){
+            $this->initRoutes();
+            $this->run($this->getUrl());
+        }
+
+        public function getRoutes(){
+            return $this->routes;
+        }
+
+        public function setRoutes(array $routes){
+            $this->routes = $routes;
+        }
+
+        protected function run($url){
+
+            //echo "*********".$url."**********";
+            foreach( $this->getRoutes() as $key => $route){
+                    
+                if($url == $route['route']){
+                    $class = "app\\controllers\\".$route['controller'];//ucfirst($route['controller']);
+                        
+                    $controller = new $class;
+        
+                        //actions
+                    $action = $route['action'];
+        
+                    $controller->$action();
+                    }
+                }
+        }
+
+        protected function getUrl(){
+            #return $_SERVER;//retorna todos os detalhaes do servidor da aplicação
+            #return parse_url($_SERVER['REQUEST_URI']);//retorna um array detalhando os componentes da URL
+            return parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);//retorna a string relativa ao path
+    
+        }
+
+    
 
     }
 
