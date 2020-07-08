5a4696f1d0a9af0c02d2206f6287b98bbb39e461
diana <diana.iruna@gmail.com>
2020-07-07 22:30:28 -0300

    views

--- Curso Web/Seção18 - Padrão de arquitetura MVC com PHP/projetos/miniframework/app/controllers/indexController.php
+++ Curso Web/Seção18 - Padrão de arquitetura MVC com PHP/projetos/miniframework/app/controllers/indexController.php
@@ -5,11 +5,23 @@ namespace app\controllers;
 class IndexController {
 
 	public function index() {
-		require_once "../app/Views/index/index.phtml";
+		$dados = array('x','yyyy','z');
+		$this->render('index',$dados);
 	}
 
 	public function sobreNos() {
-		require_once "../app/Views/index/sobreNos.phtml";
+		$dados = array('aaaa','bbbbbb','yyyyyyyyyy');
+		$this->render('sobreNos',$dados);
+	}
+
+	public function render($view,$dados){
+		//echo get_class($this);//a gente passa uma classe e ele retorna o nome de uma classe
+		$classAtual = get_class($this);
+		$classAtual = str_replace('app\\controllers\\','',$classAtual);//str_replace(1*'app\\controllers\\',2*'',3*$classAtual)
+														  //1* -> o que vc ta procurando na string/2*-> o que vc vai escrever no lugar dela/3* -> variavel onde vc vai procurar
+		$classAtual = strtolower(str_replace('Controller','',$classAtual));	
+
+		require_once "../app/Views/".$classAtual."/".$view.".phtml";
 	}
 }
 
