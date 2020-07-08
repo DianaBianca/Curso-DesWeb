7fbd295f181cc16fe9d3ee55fa1610dc91ff212d
diana <diana.iruna@gmail.com>
2020-07-07 22:45:37 -0300

    abstração dos controller DONE

--- Curso Web/Seção18 - Padrão de arquitetura MVC com PHP/projetos/miniframework/app/controllers/indexController.php
+++ Curso Web/Seção18 - Padrão de arquitetura MVC com PHP/projetos/miniframework/app/controllers/indexController.php
@@ -1,14 +1,8 @@
 <?php
 
 namespace app\controllers;
-
-class IndexController {
-
-	private $view;
-
-	public function __construct(){
-		$this->view = new \stdClass();//criando objetos vazios
-	}
+use MF\Controller\Action;
+class IndexController extends Action{
 
 	public function index() {
 		$this->view->dados = array('x','yyyy','z');
@@ -20,16 +14,7 @@ class IndexController {
 		$this->render('sobreNos');
 	}
 
-	public function render($view){
-		//echo get_class($this);//a gente passa uma classe e ele retorna o nome de uma classe
-		$classAtual = get_class($this);
-		$classAtual = str_replace('app\\controllers\\','',$classAtual);//str_replace(1*'app\\controllers\\',2*'',3*$classAtual)
-														  //1* -> o que vc ta procurando na string/2*-> o que vc vai escrever no lugar dela/3* -> variavel onde vc vai procurar
-		$classAtual = strtolower(str_replace('Controller','',$classAtual));	
-
-		require_once "../app/Views/".$classAtual."/".$view.".phtml";
-	}
+	
 }
-
 
 ?>
---
+++ Curso Web/Seção18 - Padrão de arquitetura MVC com PHP/projetos/miniframework/vendor/MF/Controller/Action.php
@@ -0,0 +1,23 @@
+<?php
+namespace MF\Controller;
+
+abstract  class Action{
+    protected $view;
+
+	public function __construct(){
+		$this->view = new \stdClass();//criando objetos vazios
+    } 
+    
+    protected function render($view){
+		//echo get_class($this);//a gente passa uma classe e ele retorna o nome de uma classe
+		$classAtual = get_class($this);
+		$classAtual = str_replace('app\\controllers\\','',$classAtual);//str_replace(1*'app\\controllers\\',2*'',3*$classAtual)
+														  //1* -> o que vc ta procurando na string/2*-> o que vc vai escrever no lugar dela/3* -> variavel onde vc vai procurar
+		$classAtual = strtolower(str_replace('Controller','',$classAtual));	
+
+		require_once "../app/Views/".$classAtual."/".$view.".phtml";
+	}
+}
+
+
+?>
