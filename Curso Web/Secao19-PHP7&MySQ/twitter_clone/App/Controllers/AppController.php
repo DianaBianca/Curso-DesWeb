364f901d260d8da86e52e3bd34f4381ae5e3bbd2
diana <diana.iruna@gmail.com>
2020-07-14 13:27:06 -0300

    seguindo e deixando de seguir outros usuarios DONE

--- Curso Web/Secao19-PHP7&MySQ/twitter_clone/App/Controllers/AppController.php
+++ Curso Web/Secao19-PHP7&MySQ/twitter_clone/App/Controllers/AppController.php
@@ -63,8 +63,8 @@ class AppController extends Action {
 		if($pesquisarPor != '') {
 			
 			$usuario = Container::getModel('Usuario');
-            $usuario->__set('nome', $pesquisarPor);
-            $usuario->__set('id',$_SESSION['id']);
+			$usuario->__set('nome', $pesquisarPor);
+			$usuario->__set('id', $_SESSION['id']);
 			$usuarios = $usuario->getAll();
 
 		}
@@ -72,24 +72,28 @@ class AppController extends Action {
 		$this->view->usuarios = $usuarios;
 
 		$this->render('quemSeguir');
-    }	
-    
-    public function acao(){
-        $this->validaAutenticacao();
+	}	
 
-        $acao = isset($_GET['acao']) ? $_GET['acao'] : '';
-        $acao = isset($_GET['id_usuario']) ? $_GET['id_usuario'] : ''; 
-        
-        $usuario = Container::getModel('Usuario');
-        $usuario->__set('id', $_SESSION['id']);
+	public function acao() {
 
-        if($acao == 'seguir'){
-            $usuario->seguirUsuario($id_usuario_seguindo);
-        }else if ($acao == 'deixar_de_seguir'){
-            $usuario->deixarSeguirUsuario();
-        }
-        header('Location: /quem_seguir');
-    }
+		$this->validaAutenticacao();
+
+		$acao = isset($_GET['acao']) ? $_GET['acao'] : '';
+		$id_usuario_seguindo = isset($_GET['id_usuario']) ? $_GET['id_usuario'] : '';
+
+		$usuario = Container::getModel('Usuario');
+		$usuario->__set('id', $_SESSION['id']);
+
+		if($acao == 'seguir') {
+			$usuario->seguirUsuario($id_usuario_seguindo);
+
+		} else if($acao == 'deixar_de_seguir') {
+			$usuario->deixarSeguirUsuario($id_usuario_seguindo);
+
+		}
+
+		header('Location: /quem_seguir');
+	}
 }
 
 ?>
