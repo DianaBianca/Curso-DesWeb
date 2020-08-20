8bf8d0fa3a28fff0e670ddff932600657127c2d5
diana <diana.iruna@gmail.com>
2020-08-20 13:52:25 -0300

    aplicando mcv

--- Curso Web/projetos/miniframework/App/Controllers/IndexController.php
+++ Curso Web/projetos/miniframework/App/Controllers/IndexController.php
@@ -20,12 +20,9 @@ class IndexController extends Action {
 	public function cadastro(){
 		$this->render('cadastro');
 
-		echo '<pre>';
-		print_r($_POST);
-		echo'</pre>';
-
 		//receber os dados do formulario
 		$usuario = Container::getModel('Usuario');
+		
 		$usuario-> __set('nome',$_POST['nome']);
 		$usuario-> __set('sobrenome',$_POST['sobrenome']);
 		$usuario-> __set('email',$_POST['email']);
