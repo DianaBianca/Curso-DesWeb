9d669c44828f3fa76f4590b99671faa5600113f8
diana <diana.iruna@gmail.com>
2020-09-07 20:06:50 -0300

    salvando agendamento no banco de dados

---
+++ Curso Web/projetos/miniframework/App/Models/Agendamentos.php
@@ -0,0 +1,17 @@
+<?php
+    namespace APP\Models;
+
+    use MF\Model\Model;
+
+    class Agendamentos extends Model{
+        private $nome_pet;
+        private $id_user;
+        private $tipo_pet;
+        private $obs;
+        private $servico;
+        private $plano;
+        private $data;
+        private $hora;
+    } 
+
+?>
