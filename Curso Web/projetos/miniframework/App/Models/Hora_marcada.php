41eb8c82a6640f067b56b90ef87086254b416cd0
diana <diana.iruna@gmail.com>
2020-09-21 16:30:55 -0300

    model Hora_Marcada

--- Curso Web/projetos/miniframework/App/Models/Hora_marcada.php
+++ Curso Web/projetos/miniframework/App/Models/Hora_marcada.php
@@ -4,6 +4,20 @@
     use MF\Model\Model;
 
     class Horamarcada extends Model{
+        private $id;
+        private $id_usuario;
+        private $nome_pet;
+        private $servico;
+        private $plano;
+        private $dia;
+        
+        public function __get($atributo){
+            return $atributo;
+        }
+
+        public function __set($atributo, $valor){
+            $this->$atributo = $valor;
+        }
 
     }
 ?>
